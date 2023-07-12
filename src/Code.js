cca61669b05505d01e2080da11ca28cf86a351c3
Your Name <kfhohn@gmail.com>
2023-07-12 12:08:48 -0400

    can add users to new table from both seed data and fresh form info

--- src/Code.js
+++ src/Code.js
@@ -3,9 +3,6 @@ const lastRevDate = new Date(2023, 5, 29);
 const ss = SpreadsheetApp.openById('1h2mZPw-CIv0ajfLYrGS0lpYUk8yAM-_OKBFK_YSTzEk');
 // const doc = DocumentApp.openById('1Yc4xXrn-XKqq_lSb9uUCUtkpers52OyHNCNsYKtVgcE');
 
-let students;
-let curStudentNetId;
-
 // render the template and serve it
 function doGet() {
   return HtmlService.createTemplateFromFile('Index').evaluate().setTitle('NYU Game Center Equipment Rental Policy');
@@ -17,27 +14,31 @@ function include(filename) {
 
 //find student's info in spreadsheet
 function lookupStudent(netid) {
-  students = ss.getSheetByName('Seed Data').getRange('A:H').getValues();
+  try {
+    students = ss.getSheetByName('Seed Data').getRange('A2:H286').getValues();
 
-  for (let student of students) {
-    Logger.log(student);
-    if (student[4] == netid) {
-      curStudentNetId = netid;
-      return student;
+    for (let student of students) {
+      if (student[4] == netid) return student;
     }
+
+    return false;
+  } catch (e) {
+    Logger.log(e);
+    return false;
   }
-
-  return false;
-}
-
-function updateStudent(data) {
-  console.log(data);
 }
 
 // function showPolicy() {
 //   return doc.getBody();
 // }
 
-function submitAgreement() {
-  return true;
+function submitAgreement(student) {
+  try {
+    let sheet = ss.getSheetByName('Registered Students');
+    sheet.appendRow(student);
+    return true;
+  } catch(e) {
+    Logger.log(e);
+    return false;
+  }
 }
