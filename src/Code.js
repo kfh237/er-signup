e4440c632c5e4141a77c4cf74b86321a3705af4a
Your Name <kfhohn@gmail.com>
2023-07-13 14:46:44 -0400

    handle case where student is already registered

--- src/Code.js
+++ src/Code.js
@@ -15,16 +15,40 @@ function include(filename) {
 //find student's info in spreadsheet
 function lookupStudent(netid) {
   try {
-    students = ss.getSheetByName('Seed Data').getRange('A2:H286').getValues();
+    let data = {
+      'type': 0,
+      'student': false
+    };
 
-    for (let student of students) {
-      if (student[4] == netid) return student;
+    let registered = ss.getSheetByName('Registered Students').getRange('A:K').getValues();
+    for (let r of registered) {
+      if (r[4] == netid) {
+        data = {
+          'type': 2,
+          'student': r
+        };
+
+        Logger.log(data);
+        return JSON.stringify(data);
+      }
     }
 
-    return false;
+    let seeds = ss.getSheetByName('Seed Data').getRange('A2:H286').getValues();
+    for (let seed of seeds) {
+      if (seed[4] == netid) {
+        data = {
+          'type': 1,
+          'student': seed
+        };
+
+        return JSON.stringify(data);
+      }
+    }
+
+    return JSON.stringify(data);
   } catch (e) {
-    Logger.log(e);
-    return false;
+    Logger.error(e);
+    return { 'type': 3, 'error': e };
   }
 }
 
@@ -34,8 +58,17 @@ function lookupStudent(netid) {
 
 function submitAgreement(student) {
   try {
-    let sheet = ss.getSheetByName('Registered Students');
-    sheet.appendRow(student);
+    student.push(new Date(), 0, 0);
+
+    MailApp.sendEmail({
+      to: student[3],
+      replyTo: 'hohn.karl@nyu.edu',
+      subject: 'Thanks for registering!',
+      body: "Thank you for signing up with the NYU Game Center Equipment Room. You are now able to rent equipment. Contact Karl Hohn if you have any further questions."
+    });
+
+    ss.getSheetByName('Registered Students').appendRow(student);
+
     return true;
   } catch(e) {
     Logger.log(e);
